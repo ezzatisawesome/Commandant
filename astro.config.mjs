832fbc64740359bb21cb5f592bc588bba9170dc4
ezzatisawesome <ezzatsuhaime@gmail.com>
2024-08-31 16:00:07 -0700

    Adding tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,19 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
-import cesium from 'vite-plugin-cesium'; // Import the plugin for Cesium
+import cesium from "vite-plugin-cesium"; // Import the plugin for Cesium
 
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind()],
-  vite: {
-    plugins: [cesium()]
-  }
+	integrations: [
+    react(), 
+    tailwind({
+      configFile: "./tailwind.config.mjs",
+      applyBaseStyles: false
+    })
+  ],
+	vite: {
+		plugins: [cesium()],
+	},
 });
