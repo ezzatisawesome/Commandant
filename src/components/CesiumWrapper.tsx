59163b7ce4708d1ad000145ad93aae71812167b6
ezzatisawesome <ezzatsuhaime@gmail.com>
2024-08-31 18:10:13 -0700

    got the orbit plots to work

--- src/components/CesiumWrapper.tsx
+++ src/components/CesiumWrapper.tsx
@@ -1,4 +1,5 @@
 import { useEffect } from "react";
+import { useStore } from "@nanostores/react";
 import {
 	Cartesian3,
 	CesiumWidget,
@@ -20,18 +21,28 @@ import { propagate } from "../services/api";
 
 export default function Test() {
 	useEffect(() => {
-		console.log(envs.CESIUM_KEY)
-		Ion.defaultAccessToken = envs.CESIUM_KEY;
+		const asyncWrapper = async () => {
+			Ion.defaultAccessToken = envs.CESIUM_KEY;
+			const viewer = new Viewer("cesiumContainer");
+			// (viewer.creditDisplay.container as HTMLElement).style.display = "none";
 
-		const viewer = new CesiumWidget("cesiumContainer");
-		(viewer.creditDisplay.container as HTMLElement).style.display = "none";
+			const results = await propagate(0, [7641.80, 0.00000001, 100.73, 0, 0, 90], 36000, 1)
+			const satelliteStates = results.statesSat
+
+			for (let i = 0; i < satelliteStates.length; i++) {
+				const dataPoint = satelliteStates[i];
+
+				viewer.entities.add({
+					position: Cartesian3.fromElements(dataPoint[0]*1000, dataPoint[1]*1000, dataPoint[2]*1000),
+					point: { pixelSize: 1, color: Color.RED }
+				});
+			}
+		}
+
+		asyncWrapper()
 	}, [])
 
 	return (
-		<div id="cesiumContainer" className="h-screen w-screen">
-
-
-
-		</div>
+		<div id="cesiumContainer" className="h-screen w-screen" />
 	)
 }
