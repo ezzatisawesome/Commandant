318c97c1b8e6d2047712bdb89afdced89b559d30
ezzatisawesome <ezzatsuhaime@gmail.com>
2024-09-22 22:40:54 -0700

    Updating name of key

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,11 @@
 /// <reference path="../.astro/types.d.ts" />
+
+
+interface ImportMetaEnv {
+	readonly PUBLIC_CESIUM_KEY: string;
+	readonly PROPAGATE_ENDPOINT: string;
+}
+
+interface ImportMeta {
+	readonly env: ImportMetaEnv;
+}
