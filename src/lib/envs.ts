2eacb0c37c927e8c4aa79b672a9971933b6a34be
ezzatisawesome <ezzatsuhaime@gmail.com>
2024-09-22 23:00:22 -0700

    No use in having env variables

--- src/lib/envs.ts
+++ src/lib/envs.ts
@@ -1,6 +1,6 @@
 export default {
     CESIUM_KEY: import.meta.env.PUBLIC_CESIUM_KEY,
     PROPAGATE_ENDPOINT: import.meta.env.PROD 
-        ? process.env.PROPAGATE_ENDPOINT
+        ? 'https://r4bxsxsmoho7wkmd2x6km2s27q0iuueq.lambda-url.us-west-2.on.aws/'
         : 'http://127.0.0.1:5000'
 }
