e2774a1e8292bc643218b72dc62e3bd72719be4b
ezzatisawesome <ezzatsuhaime@gmail.com>
2024-09-22 22:53:31 -0700

    Fixing endpoint

--- src/lib/envs.ts
+++ src/lib/envs.ts
@@ -1,4 +1,6 @@
 export default {
     CESIUM_KEY: import.meta.env.PUBLIC_CESIUM_KEY,
-    PROPAGATE_ENDPOINT: import.meta.env.PROD ? import.meta.env.PROPAGATE_ENDPOINT : 'http://127.0.0.1:5000'
+    PROPAGATE_ENDPOINT: import.meta.env.PROD 
+        ? import.meta.env.PROPAGATE_ENDPOINT
+        : 'http://127.0.0.1:5000'
 }
--- src/services/api.ts
+++ src/services/api.ts
@@ -24,7 +24,7 @@ const propagate = async (
 	};
 
 	try {
-		const response = await fetch(`${envs.PROPAGATE_ENDPOINT}/propagate`, {
+		const response = await fetch(`${envs.PROPAGATE_ENDPOINT}`, {
 			method: "POST",
 			headers: { "Content-Type": "application/json" },
 			body: JSON.stringify(payload),
