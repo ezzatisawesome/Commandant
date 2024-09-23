f90d422ffa88ecaab69cecd0bd70489281a042b4
ezzatisawesome <ezzatsuhaime@gmail.com>
2024-09-22 22:41:42 -0700

    Fix

--- src/services/api.ts
+++ src/services/api.ts
@@ -24,7 +24,7 @@ const propagate = async (
 	};
 
 	try {
-		const response = await fetch(`${envs.API_ENDPOINT}/propagate`, {
+		const response = await fetch(`${envs.PROPAGATE_ENDPOINT}/propagate`, {
 			method: "POST",
 			headers: { "Content-Type": "application/json" },
 			body: JSON.stringify(payload),
